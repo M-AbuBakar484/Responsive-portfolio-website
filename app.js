91d71313932579dabc5b08b208db017a03cb8303
M-AbuBakar484 <m.abubakar5117@gmail.com>
2024-10-03 15:57:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots:true,
+        appendDots:'.slider_dots',
+        dotclass:'.slick-dots'
+
+    });
+});
